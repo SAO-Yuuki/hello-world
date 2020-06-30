6fae74a1a56c518881dcb0d284c5d8fb1be21abe
tree <1002340568@qq.com>
2020-06-30 10:36:32 +0800

    add datasource

---
+++ WpfApp1/WpfApp2/DataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WpfApp2
+{
+    class DataSource
+    {
+        private string[] _holeTypeY;
+
+        public ChartValues<double> Results { get; set; }
+        public DataSource()
+        {
+            //_holeTypeY = new[]
+            //{
+
+            //}
+
+        }
+    }
+}
